02190627a1a6ae279b171d131c1eb9a3c3012d0b
Miguel V <migvill@users.noreply.github.com>
2025-01-16 22:55:19 -0500

    Make use of defined types. Add initial class selector with requirements list

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import './Character.css'
-import { ATTRIBUTE_LIST } from '../../consts';
+import { ATTRIBUTE_LIST, CLASS_LIST } from '../../consts';
+import { Attributes, Class } from '../../types';
 
 function Character() {
-    const [attributeLevel, setAttributeLevel] = useState(
+    const [attributeLevel, setAttributeLevel] = useState<Attributes>(
         ATTRIBUTE_LIST.reduce((acc, curr) => {
             acc[curr] = 10
             return acc
-        }, {})
+        }, {} as Attributes)
     )
+    const [currentClass, setCurrentClass] = useState<Class | null>(null)
 
-    function handleChange(attribute, delta) {
+    function changeAttributeLevel(attribute: keyof Attributes, delta: number) {
         setAttributeLevel((prev) => {
             return {
                 ...prev,
@@ -21,17 +23,39 @@ function Character() {
 
     return (
         <div className="character">
+            <h3>Attributes</h3>
             <div className="character-attributes">
                 {
                     ATTRIBUTE_LIST.map((attr, idx) => (
                         <div className="character-attribute-row" key={idx}>
                             <span>{attr}: { attributeLevel[attr] }</span>
-                            <button onClick={() => handleChange(attr, -1)}>-</button>
-                            <button onClick={() => handleChange(attr, 1)}>+</button>
+                            <button onClick={() => changeAttributeLevel(attr as keyof Attributes, -1)}>-</button>
+                            <button onClick={() => changeAttributeLevel(attr as keyof Attributes, 1)}>+</button>
                         </div>
                     ))
                 }
             </div>
+            <h3>Classes</h3>
+            <div className="character-classes">
+                {
+                    Object.keys(CLASS_LIST).map((className) => (
+                        <div className="character-class" key={className} onClick={() => setCurrentClass(className as Class)}>
+                            {className}
+                        </div>
+                    ))
+                }
+            </div>
+
+            {currentClass && (
+                <div className="character-class-requirements">
+                    <h3>{currentClass} Requirements</h3>
+                    <ul>
+                        {Object.entries(CLASS_LIST[currentClass]).map(([attribute, level]) => (
+                            <li key={attribute}>{attribute}: {level}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 
