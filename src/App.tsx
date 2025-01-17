0e012a1bedb947f5f5cc76562c6af286357ce834
Miguel V <migvill@users.noreply.github.com>
2025-01-17 00:32:40 -0500

    Add character list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import './App.css';
-import Character from './components/Character/Character';
-
+import './App.css'
+import Character from './components/Character/Character'
+import { useState } from 'react'
 
 function App() {
+  const [characters, setCharacters] = useState([])
+
+  function addCharacter() {
+    const newCharacter = { id: Date.now() }
+    setCharacters((prevCharacters) => [
+      ...prevCharacters,
+      newCharacter
+    ])
+  }
+
+  function removeCharacter(id: number) {
+    setCharacters((prevCharacters) => 
+      prevCharacters.filter((char) => char.id !== id)
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>RPG Character Builder</h1>
       </header>
       <section className="App-section">
-        <Character />
+        <button onClick={addCharacter}>Add Character</button>
+        {characters.map((char, index) => (
+          <div key={char.id} className="character-wrapper">
+            <h2>Character {index + 1}</h2>
+            <Character />
+            <button onClick={() => removeCharacter(char.id)}>Remove Character</button>
+          </div>
+        ))}
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
